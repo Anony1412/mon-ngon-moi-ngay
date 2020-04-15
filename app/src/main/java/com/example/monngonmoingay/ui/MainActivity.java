e1bc0c53ef461ded2ccb84d51242be72d37972c4
Anony1412 <nqbao.ptit@gmail.com>
2020-04-15 09:24:24 +0700

    init project

---
+++ app/src/main/java/com/example/monngonmoingay/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.monngonmoingay.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.monngonmoingay.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
